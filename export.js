c021a34830fd56a6a11f2908e09e1e3fd4cdb40e
Roshan Varughese <40583749+Animeboynz@users.noreply.github.com>
2024-06-27 13:59:56 +1200

    Split JS with imports (#61)
    * Working Split
    * Works but no Settings
    * Fully Works
    * Reformatted file
    * Fixes Error on Protobuf Load
    * Fixes sortOrder default
    ---------

---
+++ export.js
@@ -0,0 +1,59 @@
+export function encodeToProtobuf() {
+  // Load protobuf schema
+  protobuf.load('schemas/schema-mihon.proto', function (err, root) {
+    if (err) throw err;
+
+    // Resolve Backup message type
+    var Backup = root.lookupType('Backup');
+
+    try {
+      var parsedData = window.data;
+
+      parsedData.backupCategories = parsedData.backupCategories.filter(
+        category => category.order !== -1 && category.order !== 65535
+      );
+
+      // Encode the JSON data using the protobuf schema
+      var encodedData = Backup.encode(Backup.fromObject(parsedData)).finish();
+
+      // Compress the encoded data
+      var compressedData = pako.gzip(encodedData);
+
+      // Create Blob from compressed data
+      var blob = new Blob([compressedData], { type: 'application/octet-stream' });
+
+      // Create download link
+      var downloadLink = document.createElement('a');
+      downloadLink.href = URL.createObjectURL(blob);
+      downloadLink.download = 'output.tachibk';
+
+      // Append to body to ensure it is part of the document
+      document.body.appendChild(downloadLink);
+
+      // Programmatically click the download link
+      downloadLink.click();
+
+      // Remove the download link from the document
+      document.body.removeChild(downloadLink);
+    } catch (error) {
+      console.error('Error:', error);
+      alert('Error: Invalid JSON data');
+    }
+  });
+}
+
+export function dlJSON() {
+  var parsedData = window.data;
+  parsedData.backupCategories = parsedData.backupCategories.filter(
+    category => category.order !== -1 && category.order !== 65535
+  );
+
+  var jsonString = JSON.stringify(parsedData, null, 2);
+  var blob = new Blob([jsonString], { type: 'application/json' });
+  var a = document.createElement('a');
+  a.href = URL.createObjectURL(blob);
+  a.download = 'EditedBackup';
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+}
---
+++ settings.js
@@ -0,0 +1,80 @@
+import { closeModal, showModal } from './modals.js';
+import {
+  initializeLibrary,
+  setSortOrder,
+  setFilterTracking,
+  setActiveTabId,
+  setFilterStatus,
+  setFilterSource,
+  sortOrder,
+  filterStatus,
+  filterSource,
+  filterTracking,
+} from './library.js';
+
+const sortOrderSelect = document.getElementById('sort-order');
+const filterStatusSelect = document.getElementById('filter-status');
+const filterSourceSelect = document.getElementById('filter-source');
+//const highlightTrackerCheckbox = document.getElementById('highlight-tracker');
+const filterTrackedSelect = document.getElementById('filter-tracked');
+
+//filterTrackedSelect.addEventListener('change', applySettings);
+
+export function openSettingsModal() {
+  this.firstChild.style.transform = 'rotate(90deg)';
+  sortOrderSelect.value = sortOrder;
+  for (const option of filterStatusSelect.options) {
+    if (filterStatus.includes(option.value)) {
+      option.selected = true;
+    }
+  }
+  for (const option of filterSourceSelect.options) {
+    if (option.value.split(',').every(uid => filterSource.includes(uid))) {
+      option.selected = true;
+    }
+  }
+  filterTrackedSelect.value = filterTracking;
+  showModal('settings-modal');
+}
+
+export function closeSettingsModal() {
+  document.getElementById('settings-icon').firstChild.style.transform = 'rotate(0deg)';
+  closeModal('settings-modal');
+}
+
+export function applySettings() {
+  // Store the current active tab ID before reinitializing
+  //window.activeTabId = document.querySelector('.tab-content.active').id;
+  setActiveTabId(document.querySelector('.tab-content.active').id);
+  //window.sortOrder = sortOrderSelect.value;
+  setSortOrder(sortOrderSelect.value);
+
+  var tempFilterStatus = [];
+  for (const option of filterStatusSelect.options) {
+    if (option.selected) {
+      tempFilterStatus.push(option.value);
+    }
+  }
+  setFilterStatus(tempFilterStatus);
+
+  var tempFilterSource = [];
+  for (const option of filterSourceSelect.options) {
+    if (option.selected) {
+      option.value.split(',').forEach(uid => tempFilterSource.push(uid));
+    }
+  }
+  setFilterSource(tempFilterSource);
+
+  //window.filterTracking = filterTrackedSelect.value;
+  setFilterTracking(filterTrackedSelect.value);
+
+  // Save sortOrder to local storage
+  localStorage.setItem('MBV_SortOrder', sortOrder);
+
+  //const highlightTracker = highlightTrackerCheckbox.checked;
+
+  console.log('Settings applied:', { sortOrder, filterStatus, filterSource, filterTracking });
+
+  closeSettingsModal();
+  initializeLibrary();
+}
