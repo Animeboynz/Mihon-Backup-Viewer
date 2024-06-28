b6c552b247a057b111c02fef79125f908777866d
BrutuZ <BrutuZ@users.noreply.github.com>
2024-06-28 15:51:12 -0300

    Replace Hourglass emoji with Material icon

--- scripts/library.js
+++ scripts/library.js
@@ -55,7 +55,11 @@ export function initializeLibrary() {
       const tabButton = document.createElement('button');
       tabButton.className = 'tab-button';
       tabButton.id = `btn${category.name}`;
-      tabButton.textContent = category.order === 65535 ? '⌛' : category.name;
+      tabButton.textContent = category.name;
+      if (category.order === 65535) {
+        tabButton.textContent = null;
+        addMaterialSymbol(tabButton, 'history');
+      }
 
       const badge = document.createElement('span');
       badge.className = 'badge';
