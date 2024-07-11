0905d1bdb56530f77c888cee29653528bc361b6a
BrutuZ <BrutuZ@users.noreply.github.com>
2024-07-10 22:09:51 -0300

    Strip demo data functions from production

--- .esbuild.mjs
+++ .esbuild.mjs
@@ -5,6 +5,7 @@ import * as esbuild from 'esbuild';
 await esbuild.build({
   entryPoints: ['site/*', 'site/schemas/*', 'site/img/**'],
   outdir: 'min',
+  dropLabels: ['DEV'],
   bundle: true,
   minify: true,
   sourcemap: true,
